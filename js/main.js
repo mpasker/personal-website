08037ebabaa6737228f30e2de8a2e6e79b488f2b
Matt Pasker <mdpasker@gmail.com>
2017-01-16 11:15:00 -0600

    added initial js and additional about me info.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('document').ready(function() {
+    // closes responsive menu on item click
+    $('.navbar ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+     // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+    // Disable autoscroll from Boostrap carousel
+    $('.carousel').carousel('pause');
+});
